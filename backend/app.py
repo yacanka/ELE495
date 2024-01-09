24e87f0eb55d9858b6918a5416ba9818b5e31023
yacanka <134092358+yacanka@users.noreply.github.com>
2024-01-09 13:16:11 +0300

    feat: use vision models in server

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_from_directory, Response, jsonify, render_template, request
 from flask_cors import CORS
+from vision import vision_types
 
 app = Flask(__name__)
 CORS(app)
@@ -31,6 +32,19 @@ def fruits():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(app.root_path, 'static/favicon.ico', mimetype='image/x-icon')
+
+@app.route('/vision_types')
+def get_vision_types():
+    return list(vision_types.keys())
+
+
+@app.route('/video_feed', methods=['GET'])
+def video_feed():
+    stream_type = request.args.get('type', 'normal')
+    print(stream_type)
+    stream_model = vision_types[stream_type]
+    return Response(stream_model(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=3000)
